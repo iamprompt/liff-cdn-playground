0e3701048f29de8e5ac5b37cfcfbb04d4f9c7890
Supakarn Laorattanakul <wearedprompt@gmail.com>
2025-11-17 11:53:08 +0700

    feat: add version 2.27.3

--- src/lib/liff/constants.ts
+++ src/lib/liff/constants.ts
@@ -26,7 +26,8 @@ export const LIFF_SDK_VERSIONS = [
     type: LIFFSdkVersionType.SPECIFIC,
     label: 'Specific Version',
     versions: [
-      { version: '2.27.2', latest: true },
+      { version: '2.27.3', latest: true },
+      { version: '2.27.2', latest: false },
       { version: '2.27.1', latest: false },
       { version: '2.27.0', latest: false },
       { version: '2.26.1', latest: false },
